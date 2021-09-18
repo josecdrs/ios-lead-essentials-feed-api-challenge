a5296563e6919290fdbc35c14bff99bc288ec1d4
Jose Cuadros <jmcuadros.hig@gmail.com>
2021-09-18 19:26:46 +0200

    Deliver invalidData error in RemoteFeedLoader.load if client completes successfully

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -24,7 +24,7 @@ public final class RemoteFeedLoader: FeedLoader {
 			case .failure:
 				completion(.failure(Error.connectivity))
 			case .success:
-				break
+				completion(.failure(Error.invalidData))
 			}
 		}
 	}
